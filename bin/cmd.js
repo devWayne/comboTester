ecc1c4dee22c10fa22f4b7090264f4954ae59e92
珈文 <jiawen.czq@alibaba-inc.com>
2016-02-03 19:19:54 +0800

    init

---
+++ bin/cmd.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+const exec = require('child_process').execSync;
+const log = require('nl-clilog');
+const fetch = require('node-fetch');
+
+
+var argv = require("minimist")(process.argv.slice(2), {
+  alias: {
+    'daily': 'd',
+    'publish': 'p'
+  },
+  boolean: ['daily', 'publish']
+});
+
+console.log(argv._[0]);
+
+
+
+const urlInput = argv._[0],
+      baseUrl = urlInput.split("??")[0],
+      comboUrl = urlInput.split("??")[1],
+      comboUrlList = comboUrl.split(",");
+
+comboUrlList.forEach(function(_url){
+    fetch(baseUrl+_url).then(function(res){
+        if(res.status==200){
+            log.info(baseUrl+_url);
+            log.debug('200');
+        }else{
+            log.info(baseUrl+_url);
+            log.error(res.status);
+        
+        }
+    });
+});
+
